12c895cf93fcb3fb2abbcee07cf4ced5c72457bb
Администратор <@DESKTOP-PHORFLN>
2020-09-17 09:31:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = File.CreateText(@"d:\newfile.txt");
+            writer.WriteLine("This is my new file");
+            writer.WriteLine("Do you like its format?");
+            writer.Close();
+        }
+    }
+}
